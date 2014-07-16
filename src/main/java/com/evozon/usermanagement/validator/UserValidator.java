3d8c8443117f345f30c65ee2df9b7f8e631e979e
Razvan <r.moldovan@evozon.com>
2014-07-16 18:12:23 +0100

    UserValidator.java si Validator.java

---
+++ src/main/java/com/evozon/usermanagement/validator/UserValidator.java
@@ -0,0 +1,66 @@
+package com.evozon.usermanagement.validator;
+
+import java.util.Calendar;
+import java.util.Date;
+import com.evozon.usermanagement.model.User;
+
+public class UserValidator implements Validator<User>{
+	
+	private StringBuffer errors;
+
+	@Override
+	public String validate(User user) {
+		clearErrors();
+		validateUserName(user.getUserName());
+		validateEmail(user.getEmail());
+		validateBirthdate(user.getBirthdate());
+		validatePhone(user.getPhone());
+		validateSimpleField(user.getFirstName());
+		validateSimpleField(user.getLastName());
+		validateSimpleField(user.getPassword());
+		return errors.toString();
+	}
+	
+	private void clearErrors(){
+		errors.setLength(0);
+	}
+	
+	private void validateUserName(String userName) {
+		if(userName.equals("")){
+			errors.append("Username field cannot be empty\n");
+		}
+	}
+	
+	private void validateEmail(String email) {
+		if(email.equals("")) {
+			errors.append("Email field cannot be empty\n");
+		} else {
+			if(!email.contains("@")) {
+				errors.append("Email should contain the @ symbol\n");
+			}
+		}
+	}
+	
+	private void validateBirthdate(Date birthdate){
+		Date currentTime = Calendar.getInstance().getTime();
+		if(currentTime.compareTo(birthdate) < 0){
+			errors.append("The chosen birthdate should be before today date\n");
+		}
+	}
+	
+	private void validatePhone(String phone){
+		if(phone.equals("")){
+			errors.append("The phone field should not be empty\n");
+		} else {
+			if(!phone.matches("[0-9]*")){
+				errors.append("The phone field should not contain only digits (0-9)\n");
+			}
+		}
+	}
+	
+	private void validateSimpleField(String field){
+		if(field.equals("")){
+			errors.append("There should not exist empty fields\n");
+		}
+	}
+}
